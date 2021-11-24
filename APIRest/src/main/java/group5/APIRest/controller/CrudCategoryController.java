d29f642b1aa8082a6cedd2172a0847e11b6087c6
Infernaton <lubin.pauly@gmail.com>
2021-11-24 11:44:29 +0100

    Make Controller

---
+++ APIRest/src/main/java/group5/APIRest/controller/CrudCategoryController.java
@@ -0,0 +1,33 @@
+package group5.APIRest.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categories")
+public class CrudCategoryController {
+
+    @GetMapping("/")
+    public String readAllCategories(){
+        return "";
+    }
+
+    @GetMapping("/{id}")
+    public String readCategoryById(@PathVariable("id") int id){
+        return "";
+    }
+
+    @PostMapping("/")
+    public String createCategory(){
+        return "";
+    }
+
+    @PutMapping("/{id}")
+    public String updateCategory(@PathVariable("id") int id){
+        return "";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCategory(@PathVariable("id") int id){
+        return "";
+    }
+}
---
+++ APIRest/src/main/java/group5/APIRest/controller/CrudProductController.java
@@ -0,0 +1,34 @@
+package group5.APIRest.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class CrudProductController {
+
+    @GetMapping("/")
+    public String readAllProducts(){
+        return "";
+    }
+
+    @GetMapping("/{id}")
+    public String readProductById(@PathVariable("id") int id){
+        return "";
+    }
+
+    @PostMapping("/")
+    public String createProduct(){
+        return "";
+    }
+
+    @PutMapping("/{id}")
+    public String updateProduct(@PathVariable("id") int id){
+        return "";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable("id") int id){
+        return "";
+    }
+
+}
