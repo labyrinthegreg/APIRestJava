7e872f97d58623a7a47084677c3b26eb6664b69f
Infernaton <lubin.pauly@gmail.com>
2021-11-24 13:43:53 +0100

    ReadAll products

--- APIRest/src/main/java/group5/APIRest/Services/ProductsDao.java
+++ APIRest/src/main/java/group5/APIRest/Services/ProductsDao.java
@@ -2,8 +2,11 @@ package group5.APIRest.Services;
 
 import group5.APIRest.models.Products;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @Repository
 public class ProductsDao {
@@ -13,5 +16,11 @@ public class ProductsDao {
     public int add(Products products){
         String sql = "INSERT INTO products (name , created_at , type, rating) VALUES (?, ?, ?, ?);";
         return jdbcTemplate.update(sql, products.getName(), products.getCreated_at(), products.getType(), products.getRating());
+    }
+    public List<Products> readAll(){
+        String sql = "SELECT P.name, P.type, P.rating, P.created_at" +
+                "FROM Products as P;";
+        List<Products> list = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Products.class));
+        return list;
     }
 }
--- APIRest/src/main/java/group5/APIRest/controller/CrudProductController.java
+++ APIRest/src/main/java/group5/APIRest/controller/CrudProductController.java
@@ -1,14 +1,18 @@
 package group5.APIRest.controller;
 
+import group5.APIRest.Services.ProductsDao;
+import group5.APIRest.models.Products;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class CrudProductController {
 
     @GetMapping("")
-    public String readAllProducts(){
-        return "";
+    public List<Products> readAllProducts(){
+        return new ProductsDao().readAll();
     }
 
     @GetMapping("/{id}")
