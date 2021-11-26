80faa2348668294fb983254940a4d2d6e076ffc8
Léo <leo.gregori95@gmail.com>
2021-11-26 10:03:17 +0100

    Adding java doc in the controllers

--- APIRest/src/main/java/group5/APIRest/controller/CrudCategoryController.java
+++ APIRest/src/main/java/group5/APIRest/controller/CrudCategoryController.java
@@ -1,5 +1,8 @@
 package group5.APIRest.controller;
 
+/***
+ * All imports
+ */
 import com.fasterxml.jackson.core.sym.Name;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -11,38 +14,72 @@ import group5.APIRest.models.Categories;
 import net.minidev.json.JSONObject;
 
 
-
+/***
+ * Controller for the categories
+ * With route "/categories"
+ */
 @RestController
 @RequestMapping("/categories")
 public class CrudCategoryController {
     @Autowired
     private CategoryDao categoryDao;
 
+    // Creating a default return JsonObject 
+    private JSONObject msgReturn = new JSONObject();
+
+    /***
+     * Function to get all the categories from the database
+     * @return a list with all categories from the database
+     */
     @GetMapping("")
     public List<Categories> readAllCategories(){
         return categoryDao.listAll();
     }
 
+    /***
+     * Function to get a category with a specific id from the database
+     * @param id
+     * @return a category from the database
+     */
     @GetMapping("/{id}")
     public Categories readCategoryById(@PathVariable("id") int id){
         return categoryDao.listOneById(id);
     }
 
+    /***
+     * Function to create a new category in the database 
+     * @param category
+     * @return a JsonObject with a message of success
+     */
     @PostMapping("")
-    public String createCategory(@RequestBody Categories category){
+    public JSONObject createCategory(@RequestBody Categories category){
         categoryDao.addCategory(category); 
-        return "New category created with name " + category.getName() ;
+        String message = "New product " + category.getName() +" created.";
+        return this.msgReturn.appendField("message", message);
     }
 
+    /***
+     * Function to update a category with a specific id in the database
+     * @param id
+     * @param category
+     * @return a JsonObject with a message of success
+     */
     @PutMapping("/{id}")
-    public String updateCategory(@PathVariable("id") int id, @RequestBody Categories category){
+    public JSONObject updateCategory(@PathVariable("id") int id, @RequestBody Categories category){
         categoryDao.updateCategory(id, category);
-        return "Category number " + id + " have been updated and is now named " + category.getName();
+        String message = "Category number " + id + " has been successfully updated";
+        return this.msgReturn.appendField("message", message);
     }
 
+    /***
+     * Function to delete a category with a specific id in the database
+     * @param id
+     * @return a JsonObject with a message of success
+     */
     @DeleteMapping("/{id}")
-    public String deleteCategory(@PathVariable("id") int id){
+    public JSONObject deleteCategory(@PathVariable("id") int id){
         categoryDao.deleteCategory(id);
-        return "Category number " + id + " have been deleted";
+        String message = "Category number " + id + " has been successfully deleted";
+        return this.msgReturn.appendField("message", message);
     }
 }
--- APIRest/src/main/java/group5/APIRest/controller/CrudProductController.java
+++ APIRest/src/main/java/group5/APIRest/controller/CrudProductController.java
@@ -1,27 +1,47 @@
 package group5.APIRest.controller;
 
+/***
+ * All imports
+ */
 import group5.APIRest.Services.HandleSortingInputService;
 import group5.APIRest.Services.ProductsDao;
 import group5.APIRest.models.Products;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 
+
+/***
+ * Controller for the products
+ * With route "/products"
+ */
 @RestController
 @RequestMapping("/products")
 public class CrudProductController {
     @Autowired
     private ProductsDao productsDao;
+    
+    // Creating a default return JsonObject 
+    private JSONObject msgReturn = new JSONObject();
 
+    /***
+     * Function to get all the products from the database
+     * @param asc
+     * @param desc
+     * @param type
+     * @param rating
+     * @return a list of products 
+     */
     @GetMapping("")
     public List<Products> readAllProducts(@RequestParam(required = false) String asc,
                                           @RequestParam(required = false) String desc,
                                           @RequestParam(required = false) String type,
                                           @RequestParam(required = false) String rating){
+        // Creating a string for all the parameters includes in the request
         String addToRequest = "";
-
+        // Adding the parameters for the filters and the sorts
         Map<String, String> whereClose = HandleSortingInputService.makeFilters(type,rating);
         addToRequest += productsDao.reqWhere(whereClose);
 
@@ -31,27 +51,52 @@ public class CrudProductController {
         return productsDao.readAll(addToRequest);
     }
 
+    /***
+     * Function to get a specific products with his id
+     * @param id
+     * @return a Product 
+     */
     @GetMapping("/{id}")
     public Products readProductById(@PathVariable("id") int id){
         return productsDao.readOneById(id);
     }
 
+    /***
+     * Function to create a new product in the database
+     * @param newProducts
+     * @return a JsonObject with a message of success
+     */
     @PostMapping("")
-    public String createProduct(@RequestBody Products newProducts){
+    public JSONObject createProduct(@RequestBody Products newProducts){
         productsDao.add(newProducts);
-        return "The product has been successfully added";
+        String message = "New product " + newProducts.getName() +" created.";
+        return this.msgReturn.appendField("message", message);
     }
 
+    /***
+     * Function to update a product with a specific id in the database
+     * @param id
+     * @param products
+     * @return a JsonObject with a message of success
+     */
     @PutMapping("/{id}")
-    public String updateProduct(@PathVariable("id") int id, @RequestBody Products products){
+    public JSONObject updateProduct(@PathVariable("id") int id, @RequestBody Products products){
         productsDao.updateProduct(id, products);
-        return "Product number " + id + " has been successfully updated";
+        String message = "Product number " + id + " has been successfully updated";
+        return this.msgReturn.appendField("message", message);
     }
 
+     /***
+     * Function to delete a product with a specific id in the database
+     * @param id
+     * @param products
+     * @return a JsonObject with a message of success
+     */
     @DeleteMapping("/{id}")
-    public String deleteProduct(@PathVariable("id") int id){
+    public JSONObject deleteProduct(@PathVariable("id") int id){
         productsDao.delete(id);
-        return "The product has been successfully deleted";
+        String message = "Product number " + id + " has been successfully deleted";
+        return this.msgReturn.appendField("message", message);
     }
 
 }
